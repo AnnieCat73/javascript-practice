724c37b811ee004e5921e2acd7cae88097356b58
annie_i <anniecats673@gmail.com>
2021-09-15 06:09:08 +0100

    Javascript practice Day 24

--- eobject.js
+++ eobject.js
@@ -38,9 +38,69 @@ btn.addEventListener("click", function () {
 function showMouseLocation(e){
 	console.log('x=' + e.clientX + ', y=' + e.clientY);
 }
-window.addEventListener('mousemove', showMouseLocation);*/
+window.addEventListener('mousemove', showMouseLocation);
 
 const btn = document.getElementById("btn");
 btn.addEventListener('click', function(){
 	console.log('here\'s an anonymous function');
+});
+
+
+const btn = document.getElementById("btn");
+function respondOnlyOnce () {
+  console.log("You should see this only once!");
+  btn.removeEventListener("click", respondOnlyOnce);
+};
+btn.addEventListener("click", respondOnlyOnce);
+
+const btn = document.getElementById("btn");
+const outputDiv = document.getElementById("output");
+
+function showSuccessMessage () {
+  outputDiv.innerHTML = "Success!";
+  outputDiv.style.color ="green";
+  outputDiv.style.fontSize ="30px";
+};
+btn.addEventListener("click", showSuccessMessage);
+
+
+//show coordinates of when user clicks anywhere
+function showClickLocation(e){
+	console.log('x=' + e.clientX + ', y=' + e.clientY);
+};
+window.addEventListener("click", showClickLocation);
+
+const body = document.querySelector("body");
+const btn1 = document.getElementById("btn1");
+btn.addEventListener("click", function () {
+  const div1 = document.createElement("div");
+  div1.innerText = "Hello!";
+  body.appendChild(div1);
+  
+});*/
+
+
+
+const buttons = {
+  top: 0,
+  middle: 0,
+  bottom: 0
+};
+
+const topBtn = document.getElementById("top");
+const middleBtn = document.getElementById("middle");
+const bottomBtn = document.getElementById("bottom");
+
+topBtn.addEventListener("click", function () {
+  
+  }
+  
+});
+
+middleBtn.addEventListener("click", function () {
+
+});
+
+bottomBtn.addEventListener("click", function () {
+
 });
---
+++ iife.js
@@ -0,0 +1,34 @@
+/*An immediately-invoked function expression is a function that is declared and immediately run.
+It is often abbreviated as IIFE and pronounced “iffy”.
+You can use an IIFE to keep your code out of global scope, which is awesome because:
+You won’t overwrite someone else’s variables
+Your variables won’t get over-written*/
+
+(function() {
+  const message = "Secret message!";
+  console.log(message);
+})();
+
+/*Step 1: Create an anonymous function.
+function(){}
+//Step 2: Put your function in parentheses (spaces don’t matter here).
+( function(){} )
+//Step 3: Run your function.
+( function(){} )();
+//Step 4: Put whatever you want in your function.
+(function(){
+	console.log('stuff');
+})();*/
+
+//PARAMETERS IN IIFFEs
+(function(a,b) {
+  console.log(a+b);
+})(10, 15);//25
+
+
+// From this <h3 id="whatever"></h3>
+(function(d){
+  d.getElementById('whatever').innerHTML = 'something!';
+})(document);
+//To this <h3 id="whatever">something!</h3>
+
