b6d9d8bc4828db2d4e9efd8be194f08ee8c92aa7
annie_i <anniecats673@gmail.com>
2021-12-02 06:11:44 +0000

    Javascript practice part 2, 2 Dec 2021

---
+++ Practice November/string.js
@@ -0,0 +1,5 @@
+const div = document.getElementById("div");
+const string = "Mimi, my girl cat went to the vets yesterday!";
+const replace = string.replace("girl", "sweetest");
+div.innerHTML = replace;
+
