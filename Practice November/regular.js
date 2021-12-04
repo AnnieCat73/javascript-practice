252c56f373872caf4119fc9739b30c61bc8d9969
annie_i <anniecats673@gmail.com>
2021-12-04 06:07:11 +0000

    Javascript practice 4 Dec part 2

--- Practice November/regular.js
+++ Practice November/regular.js
@@ -31,7 +31,7 @@ var result = str.replace(/hi/gi, function(currentValue){
     else{ return 'bye'; }
 });
 
-console.log(result);*/
+console.log(result);
 
 
 const str = "Hi, here's some text. Hi again. Also, hi.";
@@ -46,13 +46,54 @@ const input = document.getElementById("text");
 const btn = document.getElementById("btn");
 const div = document.querySelector(".div");
 
-const email = input.value;
+var pattern = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
+
+
+
 
 btn.addEventListener("click", function () {
-  if (email.match(/@/ig)) {
+  validateEmail(input);
+});
+
+function validateEmail(input) {
+  if (input.value.match(pattern)) {
     div.innerText = "Valid email";
+    return true;
   } else {
     div.innerText = "Invalid email";
+    return false;
+  }
+}
+
+const str = "taht taht That taht taht";
+const result = str.replace(/taht/gi, function (currentValue) {
+  if (currentValue === "taht") {
+    return "that";
+  } else {
+    return "That";
   }
 });
 
+console.log(result);*/
+
+const url = window.location.href;
+console.log(url)
+
+var pattern = /.html/ig;
+const result = url.match(pattern);
+console.log(result)
+
+const input = document.getElementById("text");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  getToPage(input);
+});
+
+function getToPage(input) {
+  if (input.value.match("business")) {
+    window.location.href = "http://www.google.com/business";
+  } else {
+    window.location.href = "http://www.google.com";
+  }
+}
