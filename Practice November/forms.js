8beb19ba454282eff710ddeac38150e0ea132f05
annie_i <anniecats673@gmail.com>
2021-12-05 06:09:35 +0000

    Javascript practice 5 Dec Part 2

--- Practice November/forms.js
+++ Practice November/forms.js
@@ -29,5 +29,40 @@ function addPremium() {
   priceSpan.innerHTML = price;
 }
 
+const radioA = document.getElementById("radioa");
+const radioB = document.getElementById("radiob");
+
+/*radioA.addEventListener("click", () => {
+  console.log(this.value);
+});
+
+radioB.addEventListener("click", () => {
+  console.log(this.value);
+});*/
+
+document.getElementById('radioa').onclick = function () {
+  console.log(this.value);
+};
+
+document.getElementById('radiob').onclick = function () {
+  console.log(this.value);
+};
+
+const green = document.getElementById("green");
+const blue = document.getElementById("blue");
+const spanDiv = document.getElementById("spandiv");
+
+green.addEventListener("click", function () {
+  spanDiv.innerHTML = "green";
+});
+
+blue.addEventListener("click", function () {
+  spanDiv.innerHTML = "blue";
+});
+
+
+
+
+
 
 
