38c18778f2967f7d510eb2c75b8b2658d52aad8e
annie_i <anniecats673@gmail.com>
2021-12-06 06:05:56 +0000

    Javascript practice Part 2

---
+++ Practice November/math.js
@@ -0,0 +1,18 @@
+//console.log(Math.floor(10 * Math.random()) + 1);
+
+const random = Math.floor(10 * Math.random() + 1);
+console.log(random);
+
+console.log(Math.floor(5.7));
+
+const randomNumber = Math.ceil(Math.random());
+console.log(randomNumber)
+
+const random10 = Math.floor(10 * Math.random() + 1);
+console.log(random10)
+
+function randomNumberAgain(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+console.log(randomNumberAgain(3, 8));
