b7a3cefa3ba19c6f4b4be781d4ef3fb84bc9c1bd
annie_i <anniecats673@gmail.com>
2021-12-07 06:06:59 +0000

    Javascript Practice 7 Dec Part 2

--- Practice November/timing.js
+++ Practice November/timing.js
@@ -1,4 +1,4 @@
-let count = 0;
+/*let count = 0;
 const sayHi = setInterval(function () {
   console.log(`Current number: ${count}`);
   count++;
@@ -22,11 +22,47 @@ setTimeout(function () {
 
 const div1 = document.getElementById("div1");
 const colors = ["red", "orange", "#ff0"];
-setInterval(function () {
-  for (let i = 0; i < colors.length; i++) {
-    div1.style.backgroundColor = colors[i];
+let i = 0;
+function changeColor() {
+  div.style.backgroundColor = colors[i];
+  i++;
+  if (i > colors.length - 1) {
+    i = 0;
   }
-}, 2000);
+
+}
+setInterval(changeColor, 2000);
+
+const div2 = document.getElementById("div2");
+let countAgain = 11;
+const counter = setInterval(timer, 2000);
+
+function timer() {
+  countAgain = countAgain - 1;
+  if (countAgain <= 0) {
+    clearInterval(counter);
+  }
+  div2.innerHTML = countAgain;
+}
+*/
+var d = new Date();
+console.log(d.getDay());
+console.log(d.getDate());
+console.log(d.getMonth());
+console.log(d.getFullYear());
+
+d.setDate(d.getDate() + 120);
+console.log(d)
+
+console.log(d.getTime());
+
+const div3 = document.getElementById("div3");
+div3.innerHTML =
+
+
+
+
+
 
 
 
