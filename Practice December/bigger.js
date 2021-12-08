8b745b955b0a4c09cc059ee5a72a5e8e74501f8a
annie_i <anniecats673@gmail.com>
2021-12-08 05:56:25 +0000

    Javascript practice 8 Dec part 2

--- Practice December/bigger.js
+++ Practice December/bigger.js
@@ -1,4 +1,4 @@
-const div = document.getElementById("div");
+/*const div = document.getElementById("div");
 const openBtn = document.getElementById("open");
 const closeBtn = document.getElementById("close");
 
@@ -8,8 +8,42 @@ const closeBtn = document.getElementById("close");
 
 closeBtn.addEventListener("click", () => {
   div.style.display = "none";
-});*/
+});
 
 openBtn.addEventListener("click", () => {
   div.classList.toggle("close");
+});*/
+
+/*
+  - basic div should show and hide
+    - we need show button and hide button
+  - should cover screen and be aligned properly
+  - should hide when people click outside it
+*/
+
+const lightbox = document.getElementById("lightbox-container");
+//const overlay = document.getElementById("overlay");
+const show = document.getElementById("show");
+const hide = document.getElementById("hide");
+
+function showLightbox() {
+  lightbox.style.display = "block";
+}
+
+function hideLightbox() {
+  lightbox.style.display = "none";
+}
+
+show.addEventListener("click", () => {
+  showLightbox();
 });
+
+hide.addEventListener("click", () => {
+  hideLightbox();
+});
+
+/*overlay.addEventListener("click", () => {
+  hideLightbox();
+});*/
+
+
