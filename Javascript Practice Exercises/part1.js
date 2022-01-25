fabc17146c86c3a7c2a7f5cb97d3e098f610aed6
Annie <devannie777@gmail.com>
2022-01-25 11:39:41 +0000

    Javascript practice 25 Jan 2022

--- Javascript Practice Exercises/part1.js
+++ Javascript Practice Exercises/part1.js
@@ -41,3 +41,41 @@ let fileName = "animation.html";
 let fileExtension = getExtension(fileName);
 console.log(fileExtension);//html
 
+/*Exercise 3:
+Write a JavaScript program to replace every character in a given string with the character
+following it in the alphabet*/
+
+
+/*Exercise 4:
+Write a JavaScript program to get the current date.
+Expected Output : mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy*/
+
+var today = new Date();//Sun Jan 23 2022
+var day = today.getDate();//23
+console.log(today)//23
+
+//so
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+today = mm+'/'+dd+'/'+yyyy;
+console.log(today);
+today = dd+'-'+mm+'-'+yyyy;
+console.log(today);
+today = dd+'/'+mm+'/'+yyyy;
+console.log(today);
+
