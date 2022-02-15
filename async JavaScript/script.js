dd8bb1904c70f8284d3955134ef7b2de9c8d35d3
Annie <devannie777@gmail.com>
2022-02-15 06:24:06 +0000

    Javascript practice 15 Feb 2022

--- async JavaScript/script.js
+++ async JavaScript/script.js
@@ -276,6 +276,8 @@ request.addEventlistener('readystatechange', () => {  //find out/get the state o
     //console.log(request, request.readyState);
     if(request.readyState === 4 && request.status === 200) { 
         console.log(request.responseText);
+    } else if(request.readyState === 4) {
+        console.log('Could not fetch the data'); //SO HERE IS STH IS WRONG AT STAGE 4 AND IT' NOT STATUS 200 GET ERROR MSG I.E 404
     }
 });
 
