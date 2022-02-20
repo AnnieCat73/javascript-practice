ccb1b98a7305b1fbcb625489ff9065765f0aaca9
Annie <devannie777@gmail.com>
2022-02-20 12:45:43 +0000

    Javascript practice 20 Feb 2022

--- async JavaScript/script.js
+++ async JavaScript/script.js
@@ -222,7 +222,158 @@ async function catchRainbow() {
 
 catchRainbow();
 
-//NET NINJA ASYNC JAVASCRIPT
+
+
+/*/////////////////////FROM HERE//////////////////////*
+Sync = happening now in order instantanously like:
+console.log(1);
+console.log(2);
+
+async - happens at a later date f.ex
+setTimeout (() => {
+    console.log(4);
+}, 3000);
+
+BUT SOMETIMES IN APIS YOU NEED TO ASYNC IN ORDER AND YOU HAVE PROMISES FOR THAT
+
+A promise is an object that may produce a single value some time in the future : either a resolved value, or a reason that it's not resolved (e.g., a network error occurred). ... Promise users can attach callbacks to handle the fulfilled value or the reason for rejection.
+The Promise object supports two properties: state and result. While a Promise object is "pending" (working), the result is undefined. When a Promise object is "fulfilled", the result is a value. When a Promise object is "rejected", the result is an error object.
+
+//1) Firstly, we use a constructor to create a Promise object:
+
+const myPromise = new Promise();
+It takes two parameters, one for success (resolve) and one for fail (reject):
+
+const myPromise = new Promise((resolve, reject) => {  
+    // condition
+});
+
+Finally, there will be a condition. If the condition is met, the Promise will be resolved, otherwise it will be rejected:
+
+const myPromise = new Promise((resolve, reject) => {  
+    let condition;  
+    
+    if(condition is met) {    
+        resolve('Promise is resolved successfully.');  
+    } else {    
+        reject('Promise is rejected');  
+    }
+});
+
+//2) then( ) for resolved Promises:
+
+If you revisit the picture at the beginning of this post, you'll see that there are 2 cases: One for resolved promises and one for rejected. If the Promise gets resolved (success case), then something will happen next (depends on what we do with the successful Promise).
+
+myPromise.then();
+The then() method is called after the Promise is resolved. Then we can decide what to do with the resolved Promise.
+
+For example, let’s log the message to the console that we got from the Promise:
+
+myPromise.then((message) => {  
+    console.log(message);
+});
+catch( ) for rejected Promises:
+However, the then( ) method is only for resolved Promises. What if the Promise fails? Then, we need to use the catch( ) method.
+
+Likewise we attach the then( ) method. We can also directly attach the catch( ) method right after then( ):
+
+myPromise.then((message) => { 
+    console.log(message);
+}).catch((message) => { 
+    console.log(message);
+});
+So if the promise gets rejected, it will jump to the catch( ) method and this time we will see a different message on the console.
+//////////////////
+
+//PROMISES - resolve sends successful value back, reject sends a rejected value back
+//Pass in a reject and a resolve function and do something with the spitted out value
+//OR STILL BE PENDING meaning that neither situation happened
+
+const promise = new Promise((resolve, reject) => {
+
+    if (true) {
+        resolve('Successful');
+    }
+
+    else {
+        reject('Error');
+    }
+});
+//GET THE DATA FROM THE PROMISE - GET THE ASYNC VALUE
+promise.then((data) => {
+    console.log(data);//Successful but if (false) on first line instead get uncaught error so to catch the error do .catch()
+}).catch((error) => {
+    console.log(error);//error
+});
+
+ */
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('one');
+    }, 50);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('two');
+    }, 100);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('three');
+    }, 500);
+});
+
+//Fetch -JSON -Fetching data from a server 
+//fetch() returns a promise - inside (url) it you will fetch a url
+//Take the data that is coming back and put it in into a value we understand like json (promise 1)...then get that data/json (promise2 - both promises chained together with .then). Once we returned this value then do this and we get a js object we can work with
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((response) => {
+    console.log(response); 
+    return response.json();//Take the data that is coming back and put it in into a value we understand like json
+})
+.then((json) => {
+    console.log(json);//then get that data/json
+});
+
+//Async/Await - HOW TO REWRITE THE ABOVE WITH THESE KEYWORDS
+//async is a way to get data
+
+async function getPosts() {
+    const response =  await fetch("https://jsonplaceholder.typicode.com/posts");//so get the data
+    const json = await response.json();//call json on the data
+    //console.log(json);//gets the data
+}
+getPosts();
+
+//BUT WITH ASYNC/AWAIT NOT CATCHING ERROR SO TO DO THAT DO THIS:
+
+async function getPosts() {
+    try {
+        const response =  await fetch("https://jsonplaceholder.typicode.com/posts");//so get the data
+        const json = await response.json();//call json on the data
+        //console.log(json);//gets the data
+    }   catch (e) { //catch the error
+        console.log(e);
+    }   finally { //so after our 'try' resolves and after our catch rejects if any we are finally going to do this
+        console.log('Always runs');
+    }
+
+
+    //THIS WOULD BE THE SAME AS ABOVE BUT IN THE Async getPosts():
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((json) => json)
+    .finally(() => {
+        console.log('Always....')
+    })
+}
+getPosts();
+
+
+//NOT NEEDED UNTIL FETCH API ----------------BELOW-------NET NINJA ASYNC JAVASCRIPT
 
 // Imp as governs how we perform tasks which take some time to complete (f.ex getting data from a database)
 //Start something and finish it later
@@ -357,7 +508,7 @@ getTodos((error, data) => {
     }
 });
 
-/////////////FETCH API - better way than above http requests - newer and simplified
+///////HERE//////FETCH API - better way than above http requests - newer and simplified
 
 //ONLY EVER REJECTED WHEN GET A NETWORK ERROR
 //IF WE TYPE A WRONG ENDPOINT OR ERROR THERE IT'S STILL RESOLVED BUT IF YOU LOOK IN DEV TOOLS
@@ -394,9 +545,80 @@ console.log(3)
 console.log(4)   
 
 
+//API  -APPLICATION PROGRAMMING INTERFACE 
+- a way to allow information from an internal or external source to interact with your program.
+
+//JSON FILES - data you pull from an API - JSON files are a type of text file used for exchanging data,EX:
+
+{
+  "statusCode": 200,
+  "message": "Quotes",
+  "pagination": {
+    "currentPage": 1,
+    "nextPage": 2,
+    "totalPages": 64
+  },
+  "totalQuotes": 64,
+  
+}
+
+Look like objects as they are JavascriptObjectNotation and have key-value pairs
+
+REST APIs
+For your program to work with an API, it needs to follow specific protocols which enable communication between the API and your program.
+The most commonly used one is called the REST API protocol.
+
+API Documentation
+To find out the API protocols, you’ll need to dive into the REST API’s documentation, like f.ex
+- What web address is the API available at? What address can your program call or request data from the API?
+- Are there parameters available to be used as part of the request? Are the parameters required or optional?
+- Is there any specific information that your program needs to send in with your request? If so, what information?
+- Is there a need for your program to authenticate itself before accessing the data? If so, how to authenticate?
+
+ENDPOINTS AND PARAMETERS 
+To access specific data from an API, you’ll need to review the documentation to find which endpoints you can use, and whether 
+the endpoint accepts parameters.
+
+Endepoints
+An endpoint is the “end” of the API address
+
+1) the URL for the API starts with:
+
+https://quote-garden.herokuapp.com/api/v3
+
+2) address alone won’t get you any data. So after the URL, you’ll add the endpoint. To get all the quotes:
+https://quote-garden.herokuapp.com/api/v3/quotes
+
+3) To get a random quote 
+https://quote-garden.herokuapp.com/api/v3/quotes/random
+
+4) Check the parameters/params available - add them after endpoint with ?
+author: string (Optional) - author=beyonce (beyonce is a string)
+genre: string (Optional)
+query: string (Optional)
+page: number (Optional)
+limit: number (Optional)
+
+So for author
+https://quote-garden.herokuapp.com/api/v3/quotes?author=beyonce
+
+If more than 1 word use %20 between the words so
+https://quote-garden.herokuapp.com/api/v3/quotes?author=maya%20angelou
+
+4) IF want multiple params then chain them together using &
+https://quote-garden.herokuapp.com/api/v3/quotes?author=maya%20angelou&limit=1
 
 
+FETCHING THE DATA - WITH FETCH()/ASYNC/AWAIT
 
+const getData = async function () {
+  const res = await fetch(
+    "https://quote-garden.herokuapp.com/api/v3/quotes?author=beyonce"
+  );
+  const data = await res.json();
+  console.log(data);
+};
+getData();
 
 
 
