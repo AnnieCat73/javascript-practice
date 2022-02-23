eff953a1fbcdaa9e5f9b221c90837b345588f990
Annie <devannie777@gmail.com>
2022-02-23 10:39:51 +0000

    Javascript practice 23 Feb 2022 part 2

--- DOM/script.js
+++ DOM/script.js
@@ -91,6 +91,8 @@ fruitParent.insertBefore(pear, banana);
 3. Pineapple
 4. Orange*/
 
+
+//Exercise
 const bilbo = document.createElement("li");
 bilbo.textContent = "Bilbo Baggins";
 
@@ -105,6 +107,108 @@ const elves = characters.querySelector(".elves");
 const glorfindel = elves.children[0];
 elves.insertBefore(legolas, glorfindel);
 
+//ADDING MULTIPLE ELEMENTS TO THE DOM
+
+//1) Change an element's innerHTML directely
+
+//Just have this in your html:
+// <ol></ol>
+
+//Then want to add 3 devs to the list
+
+const ol = document.querySelector("ol");
+ol.innerHTML = `
+  <li>Helena</li>
+  <li>Sam</li>
+  <li>Pedro</li>`;
+
+/*Same as 
+<ol>
+  <li>Addy Osmani</li>
+  <li>Vitaly Friedman</li>
+  <li>Chris Coyier</li>
+</ol> 
+
+or on your webpage
+
+  1. Helena
+  2. Sam
+  3. Pedro
+*/
+
+//In reality you will propably have an array of devs to add to the DOM
+const devs = [
+  'Addy Osmani',
+  'Vitaly Friedman',
+  'Chris Coyier'
+]
+/*To produce the HTML string, you want to use map to modify each arrayitem into a list. Then, use join to convert the array into a string.*
+const string =
+  devs.map(dev => `<li>${dev}</li>`).join('');
+
+//console.log(string)
+
+//Once you have the final HTML String, you can change the innerHTML.
+ol.innerHTML = string
+
+//WHAT IF YOU HAVE EXISTING ITEMS IN THE LIST
+
+/*In your HTML already have:
+<ol>
+  <li>Rachel Andrew</li>
+  <li>Jen Simmons</li>
+  <li>Una Kravets</li>
+</ol>*
+
+// Devs to add to <ol>
+const devs = [
+  'Addy Osmani',
+  'Vitaly Friedman',
+  'Chris Coyier'
+];
+//then add it to original ol.innerHTML
+const string = devs.map(dev => `<li>${dev}</li>`)
+  .join('')
+ol.innerHTML = ol.innerHTML + string
 
 
 
+//REMOVING ELEMENTS FROM THE DOM
+
+/*Let’s see it in action. Say you want to remove Jen Simmons from the list below.
+
+<ol>
+  <li>Rachel Andrew</li>
+  <li>Jen Simmons</li>
+  <li>Una Kravets</li>
+</ol>*
+
+const jen = document.querySelectorAll('li')[1];
+jen.parentNode.removeChild(jen);
+//PARENTNODE REFERS TO THE PARENT, cld be list as ex below
+
+//MOVING THE HTML ELEMENT
+
+/*Let’s say you want to move Jen from the second position to the first position in the following HTML.
+
+<ol>
+  <li>Rachel Andrew</li>
+  <li>Jen Simmons</li>
+  <li>Una Kravets</li>
+</ol>*
+
+const list = document.querySelector('ol');
+const rachel = list.children[0];
+
+const jen = list.removeChild(list.children[1]);
+list.insertBefore(jen, rachel);
+
+/*result is:
+
+<ol>
+  <li>Jen Simmons</li>
+  <li>Rachel Andrew</li>
+  <li>Una Kravets</li>
+</ol>*/
+
+//EXERCISES
