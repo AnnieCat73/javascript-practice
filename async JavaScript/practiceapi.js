735eabba415c4fd07c8b4d0b15f7c50e84b9567b
Annie <devannie777@gmail.com>
2022-03-01 08:18:45 +0000

    Javascript practice 1 March part 2

--- async JavaScript/practiceapi.js
+++ async JavaScript/practiceapi.js
@@ -8,7 +8,7 @@ const cat_btn = document.getElementById('cat_btn');
 const dog_btn = document.getElementById('dog_btn');
 
 cat_btn.addEventListener("click", getRandomCatFact);
-dog_btn.addEventListener("click", getRandomDogFact);
+dog_btn.addEventListener("click", getRandomDog);
 
 function getRandomCatFact () {
   fetch('https://meowfacts.herokuapp.com')
@@ -17,3 +17,12 @@ function getRandomCatFact () {
       cat_result.innerHTML = `<p>${data.data}</p>`
     })
 }
+function getRandomDog () {
+  fetch('https://randomfox.ca/floof/')
+    .then(response => response.json())
+    .then(data => {
+      dog_result.innerHTML = `<img>${data.image}</img>`
+    })
+}
+
+//with async await
