79ab02b25fced548cde230b22113a1abe3fdba00
Annie <devannie777@gmail.com>
2022-03-01 06:17:23 +0000

    Javascript practice 1 March 2022

---
+++ async JavaScript/practiceapi.js
@@ -0,0 +1,19 @@
+const cat_result = document.getElementById('cat_result');
+const dog_result = document.getElementById('dog_result');
+
+const cat_fact = document.getElementById('cat_fact');
+const dog_fact = document.getElementById('dog_fact');
+
+const cat_btn = document.getElementById('cat_btn');
+const dog_btn = document.getElementById('dog_btn');
+
+cat_btn.addEventListener("click", getRandomCatFact);
+dog_btn.addEventListener("click", getRandomDogFact);
+
+function getRandomCatFact () {
+  fetch('https://meowfacts.herokuapp.com')
+    .then(response => response.json())
+    .then(data => {
+      cat_result.innerHTML = `<p>${data.data}</p>`
+    })
+}
