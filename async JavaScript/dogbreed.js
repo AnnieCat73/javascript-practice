371a39e0681562f43bf2c58e83a28dc023ada3fd
Annie <devannie777@gmail.com>
2022-03-08 06:19:16 +0000

    Javascript practice 8 March 2022 part 2

--- async JavaScript/dogbreed.js
+++ async JavaScript/dogbreed.js
@@ -32,7 +32,7 @@ function createBreedList(breedList) {
 async function loadByBreed(breed) {
   if (breed != "Choose a dog breed") {
    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
-   const data = await response.json()
-   console.log(data)
+   const data = await response.json();
+   console.log(data);
   }
 }
