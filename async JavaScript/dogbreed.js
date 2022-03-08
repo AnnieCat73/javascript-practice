8e35e555e0e8b63d5fa5e7eae7ea2358052ba3cc
Annie <devannie777@gmail.com>
2022-03-08 06:12:47 +0000

    Javascript practice 8 March 2022

--- async JavaScript/dogbreed.js
+++ async JavaScript/dogbreed.js
@@ -20,11 +20,19 @@ start();//must do this see the data
 
 function createBreedList(breedList) {
   document.getElementById("breed").innerHTML = `
-    <select>
+    <select onchange="loadByBreed(this.value)">
       <option>Choose a dog breed</option>
         ${Object.keys(breedList).map(function (breed) {
           return `<option>${breed}</option>`
-        })}
+        }).join('')}
     </select>`;
 
+}
+
+async function loadByBreed(breed) {
+  if (breed != "Choose a dog breed") {
+   const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+   const data = await response.json()
+   console.log(data)
+  }
 }
