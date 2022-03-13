f23a5ae2e4e76597c5742027303fa1ecb0aeab60
Annie <devannie777@gmail.com>
2022-03-13 06:17:46 +0000

    Javascript practice 13 March 2022

--- async JavaScript/async practice api/dogapi.js
+++ async JavaScript/async practice api/dogapi.js
@@ -1,6 +1,18 @@
-const btn = document.getElementById("btn");
-const result = document.getElementById("dog_result");
+const dog_btn = document.getElementById("dog_btn");
+const dog_result = document.getElementById("dog_result");
+
+const cat_btn = document.getElementById("cat_btn");
+const cat_result = document.getElementById("cat_result");
 
 
-btn.addEventListener("click", getRandomDog);
+dog_btn.addEventListener("click", getRandomDog);
+cat_btn.addEventListener("click", getRandomCat);
 
+function getRandomCat() {
+  fetch('https://aws.random.cat/meow')
+    .then(res => res.json())
+    .then(data => {
+      cat_result.innerHTML = `<img src="${data.file}"/>`
+
+    })
+}
