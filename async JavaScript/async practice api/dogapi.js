3266b4b97c931ae477a3371c496eb3a39a2ccfea
Annie <devannie777@gmail.com>
2022-03-14 06:17:50 +0000

    Javascript practice 14 March 2022

--- async JavaScript/async practice api/dogapi.js
+++ async JavaScript/async practice api/dogapi.js
@@ -4,9 +4,16 @@ const dog_result = document.getElementById("dog_result");
 const cat_btn = document.getElementById("cat_btn");
 const cat_result = document.getElementById("cat_result");
 
+const dFactBtn = document.getElementById("dog-btn-fact");
+const dFactResult = document.getElementById("dog-fact-result");
+
 
 dog_btn.addEventListener("click", getRandomDog);
 cat_btn.addEventListener("click", getRandomCat);
+
+dFactBtn.addEventListener("click", getDogFacts);
+
+
 
 function getRandomCat() {
   fetch('https://aws.random.cat/meow')
@@ -16,3 +23,20 @@ function getRandomCat() {
 
     })
 }
+
+function getRandomDog() {
+  fetch('https://random.dog/woof.json')
+    .then(response => response.json())
+    .then(data => {
+      dog_result.innerHTML = `<img src="${data.url}"/>`
+    })
+}
+
+function getDogFacts() {
+  fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all')
+    .then(response => response.json())
+    .then(data => {
+      dFactResult.innerHTML = `<p>${data.fact}</p>`
+    })
+}
+
