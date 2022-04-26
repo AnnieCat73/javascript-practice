0ca37d0967a1e70fde22425878a9dcc40d85c81b
Annie <devannie777@gmail.com>
2022-04-26 14:58:29 +0100

    Javascript practice 26 April part 2

---
+++ Flavio Bootcamp/script2.js
@@ -0,0 +1,47 @@
+//form
+
+const input = document.querySelector("input");
+//or the same
+const username = document.querySelector("#username");
+//console.log(username)
+
+/*input.addEventListener("input", () => {
+  console.log(input.value);
+}) or
+input.addEventListener("input", (e) => {
+  console.log(e.target.value);
+})*
+
+//or the same
+
+username.addEventListener("change", () => {
+  console.log(username.value);
+})
+
+username.addEventListener("cut", () => {
+  console.log(username.value);
+})//what you have cut from input field gets logged in console
+
+username.addEventListener("copy", (e) => {
+  console.log(e.target.value);
+})//what you have copied from input field gets logged in console
+
+username.addEventListener("paste", (e) => {
+  console.log(e.target.value);//gets <empty string>
+})//what you have copied/cut from input field gets pasted in console
+//but if you:
+username.addEventListener("paste", () => {
+  console.log('paste');//paste gets printed in console
+})
+
+username.addEventListener("focus", (e) => {
+  console.log('focus');//logs focus
+})//logs <empty string> if use e.target.value*/
+
+username.addEventListener("blur", (e) => {
+  console.log('blur');//logs blur when click out of input field
+})//with e.target.value logs <empty string>
+
+
+
+
