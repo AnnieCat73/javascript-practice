e007da7a71fc195674cf5bfd420427d489c8437e
Annie <devannie777@gmail.com>
2022-04-28 11:42:17 +0100

    Javascript practice 28 April 2022

---
+++ Angela Bootcamp/script.js
@@ -0,0 +1,23 @@
+/* Functions allow you to write a series of instructions package it into a block of code {where the instructions begins and ends in here}
+This block of code gets executed when you call the function they are inside*/
+
+function getMilk() {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
