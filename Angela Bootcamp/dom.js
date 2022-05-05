1aca45f49e3e5ba0284f11ea3142daa029ce75cb
Annie <devannie777@gmail.com>
2022-05-05 13:19:17 +0100

    Javascript practice 5 May 2022

--- Angela Bootcamp/dom.js
+++ Angela Bootcamp/dom.js
@@ -1,2 +1,3 @@
 //Inline js f.ex <body onload="alert('Hello');">
 
+//document.getElementsByTagname("li");//gets all lis
---
+++ SleepLessYogi/script.js
@@ -0,0 +1,31 @@
+//Strings to integers
+
+
+/*Example 1
+const numStr1 = "111";
+const numStr2 = "222";
+
+function addNumbers(numStr1, numStr2) {
+  const str1 = Number(numStr1);
+  const str2 = parseInt(numStr2)
+  const total = str1 + str2;
+  return total;
+}
+console.log(addNumbers(numStr1, numStr2));/333*/
+
+
+//Example 2
+const str3 = "1";
+const str4 = "222";
+
+const numStr3 = parseInt(str3);
+const numStr4 = Number(str4);
+
+
+
+function addNumbersAgain(numStr3, numStr4) {
+  const totalNumbers = numStr3 + numStr4;
+  return totalNumbers;
+
+}
+console.log(addNumbersAgain(numStr3, numStr4));//223
