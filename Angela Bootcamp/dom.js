55b9df08ff94d4eacba428b3ed59399f6e685301
Annie <devannie777@gmail.com>
2022-05-06 07:19:22 +0100

    Javascript practice 6 May 2022

--- Angela Bootcamp/dom.js
+++ Angela Bootcamp/dom.js
@@ -1,3 +1,16 @@
 //Inline js f.ex <body onload="alert('Hello');">
 
 //document.getElementsByTagname("li");//gets all lis
+
+//Dom exercise
+const heading = document.querySelector("#heading");
+heading.classList.add("huge");
+heading.classList.remove("huge");
+heading.classList.toggle("huge");
+
+heading.innerHTML = "<em>Good Bye</em>";//gets emph goodbye
+
+document.querySelector("a").attributes;//gives you the attributes i.e href
+
+document.querySelector("a").getAttribute("href");//gets www.google.com
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
