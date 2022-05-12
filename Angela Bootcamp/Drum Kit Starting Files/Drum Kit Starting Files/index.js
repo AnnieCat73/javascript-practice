334f4d99cd538be12f6e13d402aac531ada2caf6
Annie <devannie777@gmail.com>
2022-05-12 06:40:25 +0100

    Javascript practice 11 May 2022

--- Angela Bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Angela Bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -52,6 +52,41 @@ for (let i = 0; i < numberOfDrumButtons.length; i++) {
   numberOfDrumButtons[i].addEventListener("click", function () {
     //console.log(this.innerHTML);//gets the button that gets clicked
     //this.style.color = "#fff";//changes the btn you press to color #
+
+    const buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+        const tom1 = new Audio(`sounds/tom-1.mp3`);
+        tom1.play();
+        break;
+      case "a":
+        const tom2 = new Audio(`sounds/tom-2.mp3`);
+        tom2.play();
+        break;
+      case "s":
+        const tom3 = new Audio(`sounds/tom-3.mp3`);
+        tom3.play();
+        break;
+      case "d":
+        const tom4 = new Audio(`sounds/tom-4.mp3`);
+        tom4.play();
+        break;
+      case "j":
+        const snare = new Audio(`sounds/snare.mp3`);
+        snare.play();
+        break;
+      case "k":
+        const crash = new Audio(`sounds/crash.mp3`);
+        crash.play();
+        break;
+      case "l":
+        let kick = new Audio(`sounds/kick-bass.mp3`);
+        kick.play();
+        break;
+
+      default: console.log(buttonInnerHTML)
+
+    }
   });
 
 }
