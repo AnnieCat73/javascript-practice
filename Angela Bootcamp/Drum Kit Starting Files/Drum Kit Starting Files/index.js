524fc97e36248298af7929fb31fc7bed3e8ec93a
Annie  Indreiten <devannie777@gmail.com>
2022-05-14 09:51:28 +0100

    Javascript practice 14 May 2022

--- Angela Bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Angela Bootcamp/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -44,7 +44,7 @@ function calculator(num1, num2, operator) {//3rd arg is function passe
 debugger;
 calculator(4, 5, multiply);//9 or can do add etc*/
 
-
+//a) DETCTING BUTTON PRESS
 
 const numberOfDrumButtons = document.querySelectorAll(".drum");
 
@@ -53,43 +53,63 @@ for (let i = 0; i < numberOfDrumButtons.length; i++) {
     //console.log(this.innerHTML);//gets the button that gets clicked
     //this.style.color = "#fff";//changes the btn you press to color #
 
-    const buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        const tom1 = new Audio(`sounds/tom-1.mp3`);
-        tom1.play();
-        break;
-      case "a":
-        const tom2 = new Audio(`sounds/tom-2.mp3`);
-        tom2.play();
-        break;
-      case "s":
-        const tom3 = new Audio(`sounds/tom-3.mp3`);
-        tom3.play();
-        break;
-      case "d":
-        const tom4 = new Audio(`sounds/tom-4.mp3`);
-        tom4.play();
-        break;
-      case "j":
-        const snare = new Audio(`sounds/snare.mp3`);
-        snare.play();
-        break;
-      case "k":
-        const crash = new Audio(`sounds/crash.mp3`);
-        crash.play();
-        break;
-      case "l":
-        let kick = new Audio(`sounds/kick-bass.mp3`);
-        kick.play();
-        break;
-
-      default: console.log(buttonInnerHTML)
-
-    }
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
   });
 
 }
+
+//b) DETECTING KEYBOARD PRESS
+
+//don't need to listen of letters on keyboard to be pressed
+//can use window.document to listen for it//entire webpage listens
+//for it
+document.addEventListener("keydown", function (e) {
+  //which key was pressed - use e so e.key to get a key
+  //console.log(e.key)
+  makeSound(e.key);
+});
+
+
+function makeSound(key) {
+
+
+  switch (key) {
+    case "w":
+      const tom1 = new Audio(`sounds/tom-1.mp3`);
+      tom1.play();
+      break;
+    case "a":
+      const tom2 = new Audio(`sounds/tom-2.mp3`);
+      tom2.play();
+      break;
+    case "s":
+      const tom3 = new Audio(`sounds/tom-3.mp3`);
+      tom3.play();
+      break;
+    case "d":
+      const tom4 = new Audio(`sounds/tom-4.mp3`);
+      tom4.play();
+      break;
+    case "j":
+      const snare = new Audio(`sounds/snare.mp3`);
+      snare.play();
+      break;
+    case "k":
+      const crash = new Audio(`sounds/crash.mp3`);
+      crash.play();
+      break;
+    case "l":
+      let kick = new Audio(`sounds/kick-bass.mp3`);
+      kick.play();
+      break;
+
+    default: console.log(buttonInnerHTML)
+
+  }
+}
+
+
 
 //const audio = new Audio(`sounds/tom-1.mp3`);
 //audio.play();
