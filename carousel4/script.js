f0ae6050e8a81f4aed7ae5dd051c8092c156fde9
Annie  Indreiten <devannie777@gmail.com>
2022-06-01 11:11:32 +0100

    Javascript practice 1 June 2022

---
+++ carousel4/script.js
@@ -0,0 +1,23 @@
+//Get variables
+
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);//track.querySelectorAll(".carouel__slide")
+
+const prevBtn = document.querySelector(".carousel__btn--left");
+const nextBtn = document.querySelector(".carousel__btn--right");
+
+const dotsNav = document.querySelector(".carousel__nav");
+const dots = dotsNav.querySelectorAll(".carousel__indicator");//Array.from(dotsNav.children)
+
+
+
+//get slidewidth plus loop for slides - position slides
+
+//click on left btn
+
+
+//click on right btn
+
+
+//working the dots - when a dot is clicked we find the corresponding slide
+
