a6b6523fd27d1fb577b78cf678cf04a88e2b182f
Annie  Indreiten <devannie777@gmail.com>
2022-06-03 05:26:54 +0100

    Javascript practice 3 June 2022

--- carousel4/script.js
+++ carousel4/script.js
@@ -12,11 +12,7 @@ const dots = dotsNav.querySelectorAll(".carousel__indicator");//Array.from(dotsN
 
 
 //get slidewidth plus loop for slides - position slides
-const slideWidth = slides[0].getBoundingClientRect().width;
 
-slides.forEach((slide, index) => {
-  slide.style.left = slideWidth * index + 'px';
-})
 
 //click on left btn
 nextBtn.addEventListener('click', e => {
@@ -38,7 +34,11 @@ nextBtn.addEventListener('click', e => {
     nextBtn.setAttribute('hidden', true);
   }
 
-  //dots
+  //updating dots when clicking on next button
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const nextDot = currentDot.nextElementSibling;
+  currentDot.classList.remove("current-slide");
+  nextDot.classList.add("current-slide");
 
 })
 
@@ -58,11 +58,63 @@ prevBtn.addEventListener('click', e => {
   if (!prevSlide.previousElementSibling) {
     prevBtn.setAttribute('hidden', true);
   }
+  //updating dots when clicking on next button
+  const currentDot = dotsNav.querySelector(".current-slide");
+  const prevDot = currentDot.previousElementSibling;
+  currentDot.classList.remove("current-slide");
+  prevDot.classList.add("current-slide");
 })
 
 
+const slideWidth = slides[0].getBoundingClientRect().width;
 
+slides.forEach((slide, index) => {
+  slide.style.left = slideWidth * index + 'px';
+})
 
 
 //working the dots - when a dot is clicked we find the corresponding slide
+
+dots.forEach(dot => {
+  dot.addEventListener("click", e => {
+    let clickedDotIndex;
+    for (let index = 0; index < dots.length; index++) {
+      if (dots[index] === dot) {
+        clickedDotIndex = index;
+      }
+    }
+    //use clidkedDotIndex to find slide to show
+    const slideToShow = slides[clickedDotIndex];
+    const destination = getComputedStyle(slideToShow).left;
+    track.style.left = "-" + destination;
+
+    //update location of .is-selected
+    slides.forEach(slide => {
+      slide.classList.remove("current-slide");
+    })
+    //then add .is-selected to newly displayed slide
+    slideToShow.classList.add("current-slide");
+
+    //update dot state so the selected dot is black
+    dots.forEach(dot => {
+      dot.classList.remove("current-slide");
+    })
+    dot.classList.add("current-slide");
+
+    if (clickedDotIndex === 0) {
+      prevButton.setAttribute("hidden", true);
+      nextButton.removeAttribute("hidden");
+    } else if (clickedDotIndex === dots.length - 1) {
+      prevButton.removeAttribute("hidden");
+      nextButton.setAttribute("hidden", true);
+    } else {
+      prevButton.removeAttribute("hidden");
+      nextButton.removeAttribute("hidden");
+    }
+
+
+
+
+  })
+})
 
