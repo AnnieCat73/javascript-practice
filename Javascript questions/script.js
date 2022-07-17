c4052309f9b189b3f130ed6a67c4fa1f1211a1b1
Annie  Indreiten <devannie777@gmail.com>
2022-07-17 05:54:02 +0100

    Javascript practice 17 July 2022

--- Javascript questions/script.js
+++ Javascript questions/script.js
@@ -1989,3 +1989,36 @@ function Func_Hoisted() {
 }
 
 
+Question #106: 
+
+How to implement Bubble sort in JavaScript?
+
+Answer 
+
+The below given code snippet will perform Array sorting using the bubble sort algorithm. */
+
+function bblSort(arr) {
+
+  for (var i = 0; i < arr.length; i++) {
+
+    // Last i elements are already in place 
+    for (var j = 0; j < (arr.length - i - 1); j++) {
+
+      // Checking if the item at present iteration 
+
+      // is greater than the next iteration 
+      if (arr[j] > arr[j + 1]) {
+
+        // If the condition is true then swap them 
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  // Print the sorted array 
+  console.log(arr);
+}
+// This is our unsorted array 
+var arr = [234, 43, 55, 63, 5, 6, 235, 547];
+bblSort(arr);
