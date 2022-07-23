bb511910c8f720d5db6bd7c93eb7a4a75944fb8a
Annie  Indreiten <devannie777@gmail.com>
2022-07-23 05:56:21 +0100

    Javascript practice 23 July 2022

--- CodeDamn/script.js
+++ CodeDamn/script.js
@@ -1,4 +1,4 @@
-// write your code below
+/* write your code below
 function performMath(number1, number2, operation) {
   if (operation === '+') {
     return number1 + number2;
@@ -8,4 +8,25 @@ function performMath(number1, number2, operation) {
 
   }
 }
-console.log(performMath(4, 2, '*'));//8
+console.log(performMath(4, 2, '*'));//8*/
+
+
+
+const incrementBtn = document.querySelector('#increment');
+const decrementBtn = document.querySelector('#decrement');
+const counterEl = document.querySelector('.counter');
+let counter = 0;
+
+function incrementCounter() {
+  counter++;
+  counterEl.innerText = counter;
+}
+
+function decrementCounter() {
+  counter--;
+  counterEl.innerText = counter;
+}
+
+incrementBtn.addEventListener('click', incrementCounter);
+decrementBtn.addEventListener('click', decrementCounter);
+
