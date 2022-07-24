aa1e4092ecd0fe5a2c9133223f62165f2d6ac0e4
Annie  Indreiten <devannie777@gmail.com>
2022-07-24 05:55:20 +0100

    Javascript practice 24 July 2022

--- CodeDamn/script.js
+++ CodeDamn/script.js
@@ -20,13 +20,52 @@ let counter = 0;
 function incrementCounter() {
   counter++;
   counterEl.innerText = counter;
+
+
 }
 
 function decrementCounter() {
   counter--;
   counterEl.innerText = counter;
+
+
 }
 
 incrementBtn.addEventListener('click', incrementCounter);
 decrementBtn.addEventListener('click', decrementCounter);
+
+
+//Destructuring
+const arr1 = [1, 2, 3]
+const arr2 = [4, 5, 6]
+
+const arr3 = [...arr1, ...arr2]
+
+console.log(arr3);//Array(6) [ 1, 2, 3, 4, 5, 6 ]
+
+const arr4 = [{
+  name: 'mehul',
+}]
+const arr5 = [{
+  name: 'John',
+}]
+
+const arr6 = [...arr4, ...arr5]
+console.log(arr6)//Array [ {"name:" "mehul"}, {"name": "john"} ]
+
+const obj1 = {
+  name: 'mehul'
+}
+const obj2 = {
+  age: '22'
+}
+
+const obj3 = {
+  ...obj1,
+  ...obj2
+}
+
+console.log(obj3)//Object { name: "mehul", age: "22" }
+
+
 
