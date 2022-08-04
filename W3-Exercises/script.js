7a92eac665883a978cb3e17bff40795945dbe416
Annie  Indreiten <devannie777@gmail.com>
2022-08-04 05:49:02 +0100

    Javascript practice 4 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -149,3 +149,28 @@ console.log(toSafeInteger('5.52'));//6
 console.log(toSafeInteger(Infinity)); //9007199254740991
 
 
+//Write a JavaScript function to convert a decimal number to binary, hexadecimal or octal number.
+
+decToBho = function (n, base) {
+
+  if (n < 0) {
+    n = 0xFFFFFFFF + n + 1;
+  }
+  switch (base) {
+    case 'B':
+      return parseInt(n, 10).toString(2);
+      break;
+    case 'H':
+      return parseInt(n, 10).toString(16);
+      break;
+    case 'O':
+      return parseInt(n, 10).toString(8);
+      break;
+    default:
+      return ("Wrong input.........");
+  }
+}
+
+console.log(decToBho(120, 'B'));//1111000
+console.log(decToBho(120, 'H'));//78
+console.log(decToBho(120, 'O'));//170
