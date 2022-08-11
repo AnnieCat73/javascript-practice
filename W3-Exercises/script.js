3bf6b3d68db32149cc02b3f25d06297ebda602ab
Annie  Indreiten <devannie777@gmail.com>
2022-08-11 05:50:11 +0100

    Javascript practice 11 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -262,3 +262,19 @@ function subtraction(arr) {
     return false;
 }
 console.log(subtraction([7, 3, 2, -1]));//3
+
+
+//Write a JavaScript function to get the current date.
+
+var curday = function (sp) {
+  today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //As January is 0.
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  return (mm + sp + dd + sp + yyyy);
+};
+console.log(curday('/'));
+console.log(curday('-'));
