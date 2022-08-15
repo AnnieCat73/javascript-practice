85994db035a12ea75b8bee24b0a1e26c75a91bf7
Annie  Indreiten <devannie777@gmail.com>
2022-08-15 06:01:33 +0100

    Javascript practice 15 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -343,3 +343,23 @@ function Trim(str) {
   }
 }
 console.log(Trim(' w3resource '));//w3resource
+
+
+//Write a JavaScript program to search a date within a string.
+
+function is_dateString(str) {
+  regexp = /^(1[0-2]|0?[1-9])\/(3[01]|[12][0-9]|0?[1-9])\/(?:[0-9]{2})?[0-9]{2}$/;
+
+  if (regexp.test(str)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(is_dateString("01/01/2015"));//true
+
+console.log(is_dateString("01/22/2015"));//true
+
+console.log(is_dateString("32/01/2015"));//false
