d4aa11090adc0a81da550aea1fea76ac9e244d0b
Annie  Indreiten <devannie777@gmail.com>
2022-08-18 06:09:57 +0100

    Javascript practice 18 August part 2 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -418,3 +418,21 @@ function is_timeString(str) {
 console.log(is_timeString("11:35:30"));//true
 
 console.log(is_timeString("90:90:90"));//false
+
+
+//Write a JavaScript function to check whether a given value is UK Post Code or not
+
+function is_ukPostCode(str) {
+  regexp = /^[A-Z]{1,2}[0-9RCHNQ][0-9A-Z]?\s?[0-9][ABD-HJLNP-UW-Z]{2}$|^[A-Z]{2}-?[0-9]{4}$/;
+
+  if (regexp.test(str)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(is_ukPostCode("B294HJ"));//true
+
+console.log(is_ukPostCode("7892"));//false
