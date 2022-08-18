840ab4dfa268c1e42efd8ee01e5aec6dc49cb521
Annie  Indreiten <devannie777@gmail.com>
2022-08-18 06:06:02 +0100

    Javascript practice 18 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -401,4 +401,20 @@ console.log(is_alphaNumeric("37828sad"));//true
 console.log(is_alphaNumeric("3243#$sew"));//false
 
 
-//
+//Write a JavaScript function to check whether a given value is time string or not.
+
+
+function is_timeString(str) {
+  regexp = /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9]):([0-5]?[0-9])$/;
+
+  if (regexp.test(str)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(is_timeString("11:35:30"));//true
+
+console.log(is_timeString("90:90:90"));//false
