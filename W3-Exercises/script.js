5d5d5f51d35d0a4ae8ddeb67aaee749fd939e524
Annie  Indreiten <devannie777@gmail.com>
2022-08-22 06:08:29 +0100

    Javascript practice 22 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -535,3 +535,16 @@ function is_alphaDash(str) {
 console.log(is_alphaDash('12-133'));//true
 
 console.log(is_alphaDash('100_23'));//true
+
+
+//Write a JavaScript function to print an integer with commas as thousands separators.
+
+function thousands_separators(num) {
+  var num_parts = num.toString().split(".");
+  num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  return num_parts.join(".");
+}
+
+console.log(thousands_separators(1000));//1,000
+console.log(thousands_separators(10000.23));//10,000.23
+console.log(thousands_separators(100000));//100,000
