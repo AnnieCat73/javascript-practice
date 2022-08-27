eb46cec46847daa8bd3367f1c687b30518a93720
Annie  Indreiten <devannie777@gmail.com>
2022-08-27 06:07:10 +0100

    Javascript practice 27 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -620,3 +620,17 @@ console.log(is_object('bar'));//false
 
 console.log(is_object(72));//false
 
+
+//Write a JavaScript function to validate whether a given value is object or not.
+
+function is_object(value) {
+  var datatype = typeof value;
+  return datatype === 'function' || datatype === 'object' && !!value;
+}
+
+console.log(is_object({ name: 'Robert' }));//true
+
+console.log(is_object('bar'));//false
+
+console.log(is_object(72));//false
+
