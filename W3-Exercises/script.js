d75889e874f7646b8a1d19f19289f9d6d70e20e2
Annie  Indreiten <devannie777@gmail.com>
2022-08-30 04:27:42 +0100

    Javascript practice 30 August 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -659,3 +659,15 @@ console.log(is_regexp(/test/));//true
 console.log(is_regexp('bar'));//false
 
 console.log(is_regexp(72));//false
+
+
+//Write a JavaScript function to validate whether a given value type is char or not. 
+
+function is_char(value) {
+  if (Object.prototype.toString.call(value) !== '[object String]')
+    return false;
+  return value && value.length === 1;
+}
+
+console.log(is_char('f'));//true
+console.log(is_char('*'));//true
