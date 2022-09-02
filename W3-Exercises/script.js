8245f277b047e6bc623a7198c7eb8cbe15070709
Annie  Indreiten <devannie777@gmail.com>
2022-09-02 06:12:32 +0100

    Javascript practice 2 September 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -703,7 +703,7 @@ console.log(thousands_separators(100000));//100,000
 
 //Write a JavaScript program that accept two integers and display the larger.
 
-var num1, num2;
+//var num1, num2;
 num1 = window.prompt("Input the First integer", "0");
 num2 = window.prompt("Input the second integer", "0");
 
@@ -772,3 +772,36 @@ else if (z > x && z > y) {
   }
 }
 //4, 0, -1
+
+
+//Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+
+for (var x = 0; x <= 15; x++) {
+  if (x === 0) {
+    console.log(x + " is even");
+  }
+  else if (x % 2 === 0) {
+    console.log(x + " is even");
+  }
+  else {
+    console.log(x + " is odd");
+  }
+}
+
+/*
+0 is even
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
+11 is odd
+12 is even
+13 is odd
+14 is even
+15 is odd*/
