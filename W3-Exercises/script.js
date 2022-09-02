a47c0a0fe7b0c2e98eae9e665a6fb3b0bb030b03
Annie  Indreiten <devannie777@gmail.com>
2022-09-02 06:06:51 +0100

    Javascript practice 2 September 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -738,3 +738,37 @@ else if (x < 0 && y > 0 && z < 0) {
 else {
   console.log("The sign is -");
 }//The sign is -
+
+
+//Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor
+//Sample numbers : 0, -1, 4
+//Output : 4, 0, -1
+
+var x = 0;
+var y = -1;
+var z = 4;
+if (x > y && x > z) {
+  if (y > z) {
+    console.log(x + ", " + y + ", " + z);
+  }
+  else {
+    console.log(x + ", " + z + ", " + y);
+  }
+}
+else if (y > x && y > z) {
+  if (x > z) {
+    console.log(y + ", " + x + ", " + z);
+  }
+  else {
+    console.log(y + ", " + z + ", " + x);
+  }
+}
+else if (z > x && z > y) {
+  if (x > y) {
+    console.log(z + ", " + x + ", " + y);
+  }
+  else {
+    console.log(z + ", " + y + ", " + x);
+  }
+}
+//4, 0, -1
