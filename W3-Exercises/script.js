e3c2e411c278aa3cc2b2156c496cbdc781b14c44
Annie  Indreiten <devannie777@gmail.com>
2022-09-06 06:09:03 +0100

    Javascript-practice 6 September 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -983,3 +983,26 @@ three_digit_armstrong_number();
 //370
 //371
 //407
+
+
+//Write a JavaScript program to construct the following pattern, using a nested for loop
+
+
+
+var x, y, chr;
+for (x = 1; x <= 6; x++) {
+  for (y = 1; y < x; y++) {
+    chr = chr + ("*");
+  }
+  console.log(chr);
+  chr = '';
+}
+
+/*
+undefined
+*
+**
+***
+****
+*****
+*/
