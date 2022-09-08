8ae583f66e59fd3637f279a61e07613c59b1ad18
Annie  Indreiten <devannie777@gmail.com>
2022-09-08 06:44:11 +0100

    Javascript-practice 8 September 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -1016,3 +1016,14 @@ for (var x = 0; x < 1000; x++) {
   }
 }
 console.log(sum);//233168
+
+
+//1. Write a JavaScript function to check whether an `input` is an array or not.
+
+is_array = function (input) {
+  if (toString.call(input) === "[object Array]")
+    return true;
+  return false;
+};
+console.log(is_array('w3resource'));//false
+console.log(is_array([1, 2, 4, 0]));//true
