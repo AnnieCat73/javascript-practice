7903f2da12b1f58d36490223feb8b04de9412857
Annie  Indreiten <devannie777@gmail.com>
2022-09-20 05:30:36 +0100

    Javascript practice 20 September 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -1212,5 +1212,27 @@ function display_array() {
 }
 
 
+//Write a JavaScript program to remove duplicate items from an array (ignore case sensitivity). 
+
+function removeDuplicates(num) {
+  var x,
+    len = num.length,
+    out = [],
+    obj = {};
+
+  for (x = 0; x < len; x++) {
+    obj[num[x]] = 0;
+  }
+  for (x in obj) {
+    out.push(x);
+  }
+  return out;
+}
+var Mynum = [1, 2, 2, 4, 5, 4, 7, 8, 7, 3, 6];
+result = removeDuplicates(Mynum);
+console.log(Mynum);//[1,2,2,4,5,4,7,8,7,3,6]
+console.log(result);//["1","2","3","4","5","6","7","8"]
+
+
 
 
--- creative-js/script.js
+++ creative-js/script.js
@@ -20,3 +20,25 @@ const user = {
 
 console.log(user.name);
 user.greet();
+
+
+//forEach, for Of instead of for
+
+const names = ['Sally', 'Punk']
+
+/*for (let name of names) {
+  if(name === 'Punk')
+}*/
+for (let name of names) {
+  console.log(name);
+  break;
+}
+
+// but can't access index
+
+
+//specifically for ARRAYS ONLY forEach can get index
+
+names.forEach(function (name, index) {
+  console.log(name, index);
+}) 
