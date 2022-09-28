87aeeeac5bc7e08b068e1dfda0963492bf58e2ae
Annie  Indreiten <devannie777@gmail.com>
2022-09-28 05:39:20 +0100

    Javascript practice 28 September 2022

--- W3-Exercises/script.js
+++ W3-Exercises/script.js
@@ -1381,3 +1381,19 @@ console.log(is_date(new Date(86400000)));//true
 console.log(is_date(new Date(99, 5, 24, 11, 33, 30, 0)));//true
 console.log(is_date([1, 2, 4, 0]));//false
 
+
+//Write a JavaScript function to get the current date. 
+
+var curday = function (sp) {
+  today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //As January is 0.
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) dd = '0' + dd;
+  if (mm < 10) mm = '0' + mm;
+  return (mm + sp + dd + sp + yyyy);
+};
+console.log(curday('/'));
+console.log(curday('-'));
+
