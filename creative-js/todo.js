f1ac68165babd7eb82ae7b98c420f69245ea025a
Annie  Indreiten <devannie777@gmail.com>
2022-10-06 05:47:37 +0100

    Javascript practice 6 October 2022

--- creative-js/todo.js
+++ creative-js/todo.js
@@ -2,11 +2,14 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 
 //Event Listeners
 
-todoButton.addEventListener('click', addTodo)
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
 //Functions
@@ -39,4 +42,50 @@ function addTodo(e) {
   todoInput.value = "";
 
 
+}
+
+function deleteCheck(e) {
+  //console.log(e.target)
+  const item = e.target;
+  //Delete
+  if (item.classList[0] === 'trash-btn') {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+      todo.remove()
+    })
+
+  }
+
+  //Check mark
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case "all":
+        todo.style.display = 'flex';
+        break;
+      case "completed":
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+    }
+  });
 }
