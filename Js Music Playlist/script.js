95bfd42fbee1639887893a9a4a550283cbf7ad08
Annie  Indreiten <devannie777@gmail.com>
2022-10-19 04:37:18 +0100

    Javascript practice 19 October 2022

--- Js Music Playlist/script.js
+++ Js Music Playlist/script.js
@@ -12,33 +12,33 @@ const songs = [
   },
   {
     id: '2',
-    songName: `Alan Walker-Fade <br>
+    songName: `Silly Love Songs <br>
     <p class="subtitle">Alan Walker</p>`,
-    poster: "images/pexels-karolina-grabowska-4498553.jpg"
+    poster: "images/pexels-jonathan-borba-3076516.jpg"
   },
   {
     id: '3',
-    songName: `Alan Walker-Fade <br>
+    songName: `Hello <br>
     <p class="subtitle">Alan Walker</p>`,
     poster: "images/pexels-karolina-grabowska-4498553.jpg"
   },
   {
     id: '4',
-    songName: `Alan Walker-Fade <br>
+    songName: `Fade <br>
     <p class="subtitle">Alan Walker</p>`,
-    poster: "images/pexels-karolina-grabowska-4498553.jpg"
+    poster: "images/16104017791582534645-128.png"
   },
   {
     id: '5',
-    songName: `Alan Walker-Fade <br>
+    songName: `Shout <br>
     <p class="subtitle">Alan Walker</p>`,
-    poster: "images/pexels-karolina-grabowska-4498553.jpg"
+    poster: "images/kike-vega-F2qh3yjz6Jk-unsplash.jpg"
   },
   {
     id: '6',
-    songName: `Alan Walker-Fade <br>
+    songName: `Kittycat <br>
     <p class="subtitle">Alan Walker</p>`,
-    poster: "images/pexels-karolina-grabowska-4498553.jpg"
+    poster: "images/edgar-nKC772R_qog-unsplash (1).jpg"
   },
 ]
 
@@ -47,4 +47,22 @@ Array.from(document.getElementsByClassName('song-item')).forEach((element, index
   element.getElementsByTagName('h5')[0].innerHTML = songs[index].songName;
 })
 
-let masterPlay = document.getElementById
+let masterPlay = document.querySelector('.master-play');
+let wave = document.getElementsByClassName('wave')[0];
+
+
+masterPlay.addEventListener('click', () => {
+  if (music.paused || music.currentTime <= 0) {
+    music.play();
+    masterPlay.classList.remove('bi-play-fill');
+    masterPlay.classList.add('bi-pause-fill');
+    wave.classList.add('active2');
+  } else {
+    music.pause();
+    masterPlay.classList.add('bi-play-fill');
+    masterPlay.classList.remove('bi-pause-fill');
+    wave.classList.remove('active2');
+  }
+
+})
+
